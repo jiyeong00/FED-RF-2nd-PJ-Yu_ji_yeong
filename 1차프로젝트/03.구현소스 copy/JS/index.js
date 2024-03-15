7b347b48dc42cf1e8792af05b899d880d40391eb
ji0 <buti12@naver.com>
2024-03-15 18:21:15 +0900

    페이지 넘기기

--- 1차프로젝트/03.구현소스 copy/JS/index.js
+++ 1차프로젝트/03.구현소스 copy/JS/index.js
@@ -1,41 +1,35 @@
 // 버튼이나 a에 포커스 되도록 하기 (JS실험실 참고)
 
-// const topBtn=document.querySelector(".top-btn");
+const wrap = document.getElementsByClassName('wrap')[0]; // 보일 영역
+const container = document.querySelectorAll('.cont');
+let page = 0; // 영역 포지션 초기값
+const lastPage = container.length; // 마지막 페이지
+const winW=window.innerHeight;
 
-// window.addEventListener("scroll", scrollBtn);
+console.log(lastPage);
 
-// function scrollBtn(){
-//     if(window.scrollY==800){
-//         topBtn.remove('opacity');
-//         console.log(window.scrollY);
-//     topBtn.innerHTML=`
-//     style="opacity: 1;"
-//     `;
-//    }
-// }
+let prot = 0;
+
+window.addEventListener('wheel',(e)=>{
+    e.preventDefault();
+
+    // 광휠 막기
+    if(prot) return;
+    prot = 1;
+    setTimeout(()=>prot=0,500);
+
+    if(e.deltaY > 0){
+       window.scrollTo(0,winW*page);
+       page++;
+       if(page>=3) page=2;
+    
+    }else if(e.deltaY < 0){
+        window.scrollTo(0,-(winW*page));
+        page=0;
+        if(page==-1) page=0;
+    }
 
 
-
-
-function changeLeftList(){
-    // var btnTxt = this.innerText;
-    // 1.함수호출확인
-    console.log("JS되는건가");
-
-    if(mainBtn.item(0).onclick){
-        if(btnClick<6){
-            btnClick+=1;
-            console.log('지금',btnClick);
-        }else{
-            btnClick=0;
-        }
-    }//if문
-
-}////////changeIngList()함수
-
-var btnClick=0;
-
-var mainBtn = document.querySelectorAll('.main-btn');
-var mainImg = document.querySelectorAll('.main-img');
-
-mainBtn.item(0).onclick=changeLeftList;
+    console.log(e.deltaY)
+    // wrap.style.top = page * -100 + 'vh';
+},{passive:false}); // 디폴트 기능 제거 - 스크롤
