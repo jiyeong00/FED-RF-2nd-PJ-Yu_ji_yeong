66ff7741b1f2f4ca7a4679def4d668dfa2b1ef9c
ji0 <buti12@naver.com>
2024-03-26 17:33:55 +0900

    ㄴㅇㅁㄴㅇㄴㅁㅇ

--- 1차프로젝트/03.구현소스/JS/more.js
+++ 1차프로젝트/03.구현소스/JS/more.js
@@ -1,3 +1,13 @@
 /* more.css 의 JS */
 
+// 스크롤 내려가면 스르륵 나오게 만들고 싶은데...
+
+// const topBtnImg=document.querySelector('.top-btn img');
+// const scrEvent=document.documentElement.scrollTop;
+
+// console.log('스크롤 위치',document.documentElement.scrollTop);
+
+// if(scrEvent==100){
+// topBtnImg.classList.add('.on');
+// };
 
