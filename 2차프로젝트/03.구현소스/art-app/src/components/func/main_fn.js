d57f5af49740e274c8dade3fb0138b086bc471cf
ji0 <buti12@naver.com>
2024-06-27 12:04:04 +0900

    ddd

--- 2차프로젝트/03.구현소스/art-app/src/components/func/main_fn.js
+++ 2차프로젝트/03.구현소스/art-app/src/components/func/main_fn.js
@@ -269,7 +269,7 @@ function mainFn() {
         <div class="red-box"></div>
         <div class="white-box"></div>
       </div>
-      <a href="${process.env.PUBLIC_URL+"/"+v.tit}">
+      <a href="${v.tit}">
         <img src="${process.env.PUBLIC_URL}/img/main_small_card${v.idx}.png" alt="${v.tit}" />
         <span>${v.tit}</span>
         <div class="black-box"></div>  
