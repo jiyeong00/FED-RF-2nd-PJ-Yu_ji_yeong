d49b45a37b13a3f6fe355351ea512bd1bd7d67c2
ji0 <buti12@naver.com>
2024-07-02 16:41:46 +0900

    ㄴㄴㄴㄴ

--- 2차프로젝트/03.구현소스/art-app/src/components/layout/Layout.jsx
+++ 2차프로젝트/03.구현소스/art-app/src/components/layout/Layout.jsx
@@ -1,10 +1,11 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import TopArea from "./TopArea";
 import MainArea from "./MainArea";
 import FooterArea from "./FooterArea";
 import { aCon } from "../modules/aCon";
 
 import mFn from "../func/my_function";
+import { useNavigate } from "react-router-dom";
 
 function Layout(props) {
   const rdmNum = useRef({
@@ -34,12 +35,73 @@ function Layout(props) {
       };
     });
     // console.log("최초랜덤:",rdmNum.current);
-  }, []);//////////랜덤컬러 useLayoutEffect
+  }, []); //////////랜덤컬러 useLayoutEffect
+
+  /////////////////////////////////////////////////////////////////////////
+  // 로그인관련//////////
+
+  // [ 상태관리 변수 ] //////////////
+  // 1. 로그인 상태관리변수
+  const [loginSts, setLoginSts] = useState(sessionStorage.getItem("minfo"));
+  // -> 초기값으로 세션스토리지 "minfo"를 할당함
+
+  // 2. 로그인 환영 메시지 상태변수
+  const [loginMsg, setLoginMsg] = useState(null);
+  console.log(loginMsg);
+
+  // [ 공통 함수 ] ///
+  // 1. 라우팅 이동함수
+  const goPage = useNavigate();
+  // 2. 로그인 환영메시지 생성함수
+  const makeMsg = (name) => {
+    // 유저아이콘
+    let usrIcon = ["🙍‍♂️", "🧏‍♀️", "🦸‍♂", "👨‍🎤", "🦸‍♀"];
+    // 랜덤수 : 0~4사이의 수
+    let rdm = Math.floor(Math.random() * 5);
+    // 로그인 메시지 상태변수 업데이트
+    setLoginMsg(`Welcome ${name} ${usrIcon[rdm]}`);
+  };
+
+  //3. 로그아웃 함수
+  const logoutFn = () => {
+    // 1. 로그인 상태값 null
+    setLoginSts(null);
+
+    // 2. 세션스 지우기
+    sessionStorage.removeItem("minfo");
+    // 3. 로그인 메시지 초기화
+    setLoginMsg(null);
+    // 4. 메인페이지로 돌아가기
+    goPage("/");
+  }; //////////logoutFn
+
+  // 화면 랜더링 구역
+  // -> 로그인 상태 체크
+  useEffect(() => {
+    if (sessionStorage.getItem("minfo")) {
+      let ss = sessionStorage.getItem("minfo");
+      // 로그인상태값
+      setLoginSts(ss);
+      // 로그인 메시지 업데이트
+      makeMsg(JSON.parse(ss).unm);
+    }
+  }, []);
 
   //// 코드 리턴구역 //////////////
   return (
     // <aCon.Provider value={{rdmNum,mCatSet}}>
-    <aCon.Provider value={{ rdmNum }}>
+    <aCon.Provider
+      value={{
+        rdmNum,
+        loginSts,
+        loginMsg,
+        setLoginSts,
+        setLoginMsg,
+        goPage,
+        makeMsg,
+        logoutFn,
+      }}
+    >
       {/* 1.상단영역 */}
       <TopArea />
       {/* 2.메인영역 */}
--- 2차프로젝트/03.구현소스/art-app/src/components/layout/TopArea.jsx
+++ 2차프로젝트/03.구현소스/art-app/src/components/layout/TopArea.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
 import gnbData from "../data/main_gnb_data";
 import { Link } from "react-router-dom";
 import Logo from "../modules/Logo";
 
 import mFn from "../func/my_function";
+import { aCon } from "../modules/aCon";
 
 function TopArea(props) {
+  const myCon = useContext(aCon);
+
   const [width, setWidth] = useState(window.innerWidth);
-  
+
   const handleResize = () => {
     setWidth(window.innerWidth);
   };
-  
+
   useEffect(() => {
     const topArea = mFn.qs("#header-area");
     if (!topArea) return;
@@ -49,7 +52,6 @@ function TopArea(props) {
     };
   }, []);
 
-
   return (
     <header id="header-area">
       <header className="header-area inbox">
@@ -78,11 +80,43 @@ function TopArea(props) {
             {width > 1000 && (
               <li>
                 <ol>
-                  {gnbData["회원가입"].map((v, i) => (
+                  {/* {gnbData["회원가입"].map((v, i) => (
                     <li key={i}>
                       <a href="#">{v}</a>
                     </li>
-                  ))}
+                  ))} */}
+                  {/* 회원가입, 로그인 버튼 */}
+                  {
+                    // 로그인 상태가 null일때 나옴
+                    myCon.loginSts === null && (
+                      <>
+                        <li>
+                          <Link to="/member">JOIN US</Link>
+                        </li>
+                        <li>
+                          <Link to="/login">LOGIN</Link>
+                        </li>
+                      </>
+                    )
+                  }
+                  {
+                    // 로그인 상태가 null이 아니면
+                    myCon.loginSts !== null && (
+                      <>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              myCon.logoutFn();
+                            }}
+                          >
+                            LOGOUT
+                          </a>
+                        </li>
+                      </>
+                    )
+                  }
                 </ol>
               </li>
             )}
@@ -90,7 +124,10 @@ function TopArea(props) {
               <div className="topMenu-M">
                 {/* 모바일 햄버거버튼 */}
                 <div className="nav-img">
-                  <img src={process.env.PUBLIC_URL+"/img/menu_b.png"} alt="메뉴버튼 이미지" />
+                  <img
+                    src={process.env.PUBLIC_URL + "/img/menu_b.png"}
+                    alt="메뉴버튼 이미지"
+                  />
                 </div>
                 <li className="memberMenu">
                   <ol>
