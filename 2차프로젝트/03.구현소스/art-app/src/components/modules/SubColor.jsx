77848735dc0da0b8e10b549556d57cf4a9f6c8f0
ji0 <buti12@naver.com>
2024-07-29 18:16:59 +0900

    검색 어캐하아아암

--- 2차프로젝트/03.구현소스/art-app/src/components/modules/SubColor.jsx
+++ 2차프로젝트/03.구현소스/art-app/src/components/modules/SubColor.jsx
@@ -22,8 +22,6 @@ function SubColor({ catName, subCatName }) {
   const cNum = myCon.rdmNum.current[catName][subCatName];
 
   const selData = sub_color;
-  // 필터 검색어
-  let orgData = selData;
 
   const [showButton, setShowButton] = useState(false);
   // 검색어 저장변수
@@ -53,28 +51,85 @@ function SubColor({ catName, subCatName }) {
   }, []);
 
   ////////////////////////////////////////////////////////////////////
+
+  // const thisData = selData.map((v, i) => (
+  //   <Fragment key={i}>
+  //     {i <= cNum && (
+  //       <li>
+  //         <div
+  //           className="color-box"
+  //           style={
+  //             v.code == "#FFFFFF"
+  //               ? {
+  //                   backgroundColor: v.code,
+  //                   border: "1px solid #efefef",
+  //                 }
+  //               : { backgroundColor: v.code }
+  //           }
+  //         ></div>
+  //         <p>{v.num}</p>
+  //         <p>{v.name}</p>
+  //       </li>
+  //     )}
+  //     {
+  //       i>cNum && ''
+  //     }
+  //     </Fragment>
+  // ));
+
+  const thisData = [];
+  let thisData2=[];
+  // 배열의 요소를 순회하면서 필요한 조건에 따라 처리
+  for (let i = 0; i < selData.length; i++) {
+    if (i > cNum) {
+      break; // cNum을 초과하면 반복 종료
+    }
+    thisData2[i]=selData[i];
+    thisData.push(
+      <li key={i}>
+        <div
+          className="color-box"
+          style={
+            thisData2.code === "#FFFFFF"
+              ? {
+                  backgroundColor: thisData2.code,
+                  border: "1px solid #efefef",
+                }
+              : { backgroundColor: thisData2.code }
+          }
+        ></div>
+        <p>{thisData2.num}</p>
+        <p>{thisData2.name}</p>
+      </li>
+    );
+    // console.log(thisData2);
+  }
+
+  // 필터 검색어
+  let orgData;
   // 검색할려고 만든 변수
   const searchList = () => {
     if (keyword != "") {
-      orgData = selData.filter((v) => {
+      orgData = thisData2.filter((v) => {
+        // console.log(v,v.name);
         // 소문자 처리하기
         // (1) 검색원본데이터
-        let orgTxt = v.name.toLowerCase();
+        let orgTxt = v.name;
         // (2) 검색어 데이터
-        let txt = keyword.toLowerCase();
+        let txt = keyword;
 
         // 필터검색조건 맞는 데이터 수집하기
         if (orgTxt.indexOf(txt) != -1) {
           console.log(orgTxt.indexOf(txt));
           return true;
         }
+        // console.log("필터 데이터",thisData2);
       }); ////filter
-      // console.log("필터 데이터",orgData.length,selData.length);
-      console.log("필터 데이터",orgTxt,selData.length);
+      console.log("필터 데이터",orgData);
     }
     // 검색어가 없는 경우 전체 넣기
     else {
-      orgData = selData;
+      orgData = thisData2;
     }
   };
 
@@ -94,8 +149,9 @@ function SubColor({ catName, subCatName }) {
               onClick={(e) => {
                 // 검색어 읽어오기
                 let txt = $(e.target).prev().val();
-                // console.log(txt);
                 setKeyword(txt);
+
+                console.log(thisData2);
 
                 searchList();
               }}
@@ -127,28 +183,7 @@ function SubColor({ catName, subCatName }) {
                     )}
                   </Fragment>;
                 })} */}
-              {keyword == "" &&
-                selData.map((v, i) => (
-                  <Fragment key={i}>
-                    {i <= cNum && (
-                      <li>
-                        <div
-                          className="color-box"
-                          style={
-                            v.code == "#FFFFFF"
-                              ? {
-                                  backgroundColor: v.code,
-                                  border: "1px solid #efefef",
-                                }
-                              : { backgroundColor: v.code }
-                          }
-                        ></div>
-                        <p>{v.num}</p>
-                        <p>{v.name}</p>
-                      </li>
-                    )}
-                  </Fragment>
-                ))}
+              {keyword == "" && thisData}
             </>
           </ul>
         </div>
